dc667a3c6e5dd47996afbe9f2cf744e42d5a03fe
Irina Kabaliuk <i.kabaliuk@weplay.tv>
2024-03-02 16:33:59 +0200

    hw-2

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,86 @@
+import * as contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js"
+import {createContactSchema, updateContactSchema} from "../schemas/contactsSchemas.js"
+
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await contactsService.listContacts();
+
+    res.json(result);
+  }
+  catch(error) {
+    next(error)
+  }  
+};
+
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.getContactById(id);
+
+    if (!result) {
+      throw HttpError(404)
+    }
+    res.json(result);
+  }
+  catch (error) {
+    next(error)
+  }
+};
+
+export const deleteContact = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
+
+     if (!result) {
+      throw HttpError(404)
+    }
+
+    res.json(result)
+
+  }
+  catch (error) {
+    next(error)
+  }
+};
+
+export const createContact = async (req, res) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    
+    if (error) {
+      throw HttpError(400, "data is not valid")
+    }
+
+    const result = await contactsService.addContact(req.body.name, req.body.email, req.body.phone)
+    res.status(201).json(result);
+  }
+
+  catch (error) {
+    next(error)
+  }
+};
+
+export const updateContact = async (req, res) => {
+  try {
+    const { error } = updateContactSchema.validate(req.body);
+
+    if (error) {
+      throw HttpError(400, "data is not valid")
+    }
+
+    const { id } = req.params;
+    const result = await contactsService.updateContact(id, req.body);
+
+    if (!result) {
+      throw HttpError(404, "Body must have at least one field")
+    }
+
+    res.json(result);
+  }
+
+  catch (error) {
+    next(error)
+  }
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,66 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { nanoid } from 'nanoid';
+
+const contactsPath = path.resolve('db', 'contacts.json');
+
+export const updateContacts = contacts => fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+
+
+export async function listContacts() {
+  const data = await fs.readFile(contactsPath, 'utf-8');
+
+  return JSON.parse(data);
+}
+
+export async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === contactId);
+
+  return result || null;
+}
+
+export async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+
+  contacts.push(newContact);
+  await updateContacts(contacts);
+
+  return newContact;
+}
+
+export async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId);
+
+  if (index === -1) {
+    return null
+  };
+
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+
+  return result;
+}
+
+export async function updateContact(id, data) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(contact => contact.id === id);
+
+  if (index === -1) {
+    return null;
+  }
+
+  contacts[index] = { ...contacts[index], ...data };
+  await updateContacts(contacts);
+
+  return contacts[index]
+}
+
